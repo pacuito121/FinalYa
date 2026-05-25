ec68195388f494785f083fda0d77d008b6405727
pacuito121 <corexmartex@gmail.com>
2026-05-25 11:52:13 -0600

    Add files via upload

---
+++ JS/Ads.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    backgroundImage: {
+                        'fondo-retro': 'linear-gradient(135deg,'+
+                                        '#000000 0%, #000000 10%,'+//1
+                                        ' #0B4075 10%, #0B4075 20%,'+//2
+                                        '#D93C66 20%, #D93C66 30%,'+//3
+                                        '#FAD264 30%, #FAD264 40%,'+//4
+                                        '#F2845E 40%, #F2845E 50%,'+//5
+                                        '#D93C66 50%, #D93C66 60%,'+//6
+                                        '#FAD264 60%, #FAD264 70%,'+//7
+                                        '#F2845E 70%, #F2845E 80%,'+ //8
+                                        '#0B4075 80%, #0B4075 90%,'+ //9
+                                        '#000000 90%, #000000 100%)'//10
+                    },
+                    colors: {
+                        "amascuro": "#CE8900",
+                        "amaluzo": "#FAD264",
+                        "amaclaro": "#FFFBB3",
+                        "Bazul":"#669FD3",
+                        "Barillo":"#F4D35E",
+                        "Rojino":"#D93C66",
+                        "Azulo":"#0B4075",
+                        "Fonder":"#EBEBD3",
+                        }
+                }
+            }
+        }   
